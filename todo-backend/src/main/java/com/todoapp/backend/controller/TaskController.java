4b29a37b2d640ab21342d964548af44c8319bc72
prade <prade@chintu>
2025-10-24 20:34:05 +0530

    Commit

--- todo-backend/src/main/java/com/todoapp/backend/controller/TaskController.java
+++ todo-backend/src/main/java/com/todoapp/backend/controller/TaskController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://react-todo-spring.vercel.app/")
 public class TaskController {
 
     private final TaskService service;
